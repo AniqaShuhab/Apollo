431ce5605ea5e0ba51d2d151e6ce9476a6e3e889
Aniqa Shuhab <aniqashuhab@gmail.com>
2025-01-27 15:11:38 +0500

    Update next.config.js for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',  // This is crucial for static HTML export
   basePath: '/Apollo',
-  assetPrefix: '/Apollo/',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Apollo/' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
